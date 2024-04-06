c9568ad778bc5af6271aa7b379ade4fadb7a4880
angelajasso <angelajasso@hotmail.com>
2024-04-05 18:43:16 -0600

    creating option

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.getElementById("login-form").addEventListener("submit", function(event)
     // Aquí debes agregar tu lógica de autenticación, por ejemplo, comparando el username y password con una base de datos o valores predefinidos
     if (username === "Veronica" && password === "veroAdmon") {
       // Si las credenciales son válidas, redirige al usuario a una página de bienvenida
-      window.location.href = "generadorQR.html";
+      window.location.href = "opciones.html";
     } else {
       // Si las credenciales son inválidas, muestra un mensaje de error
       document.getElementById("error-message").innerText = "Credenciales incorrectas. Por favor, intenta nuevamente.";
